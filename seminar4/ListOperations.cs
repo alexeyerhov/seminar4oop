f142dd1806f900bf8da8b3ab8b10dbdc5c7243c7
Alexey <>
2024-05-02 13:47:35 +0300

    Добавьте файлы проекта.

---
+++ seminar4/ListOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seminar4
+{
+    internal static class ListOperations
+    {
+        public static IEnumerable<T> MyWhere<T>( this IEnumerable<T> values, Func<T, bool> predicate)
+        {
+            foreach (var item in values)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ seminar4/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using seminar4;
+
+var numbers = new List<int>() { 5,6,7,8,9 };
+var numbers2 = numbers.MyWhere<int>(x => x > 7);
+
+numbers2.ToList().ForEach(Console.WriteLine);
